fc1ac35c1d93f9e9cc925fb3677f42d813baaeac
DevMilovici <devmilovici@gmail.com>
2024-11-13 17:38:04 +0200

    removed unused components. added tailwind. scss.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,40 +1,41 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/planet_icon_512x512.png" width="125" height="125" />
-
-    <div class="wrapper">
-      <nav>
+  <!-- Page header -->
+  <header class="w-[200px] min-w-[200px] max-w-[200px] flex flex-col gap-4 p-2 shadow-lg shadow-green-500/50">
+    <!-- Logo -->
+    <div class="flex flex-col items-center justify-center">
+      <!-- Title -->
+      <span class="font-bold text-2xl text-teal-400">GeoFlowCraft</span>
+      <!-- Image -->
+      <img alt="Vue logo" class="logo" src="@/assets/planet_icon_512x512.png" width="125" height="125" />
+    </div>
+    <!-- Nav links -->
+    <div class="h-full flex flex-col">
+      <nav class="flex flex-col items-center justify-center">
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
-
-  <RouterView />
+  <!-- Page content -->
+  <RouterView class="w-full" />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+
+
+export default {
+  name: "App",
+  components: {
+    RouterLink, RouterView
+  },
+  computed: {},
+  methods: {}
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+</script>
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+<style scoped lang="scss">
 
 nav a.router-link-exact-active {
   color: var(--color-text);
@@ -44,40 +45,8 @@ nav a.router-link-exact-active:hover {
   background-color: transparent;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
 nav a:first-of-type {
   border: 0;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
