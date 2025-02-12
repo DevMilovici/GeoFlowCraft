d350dbefab9671f0a080fe29e26ba09a4130fb82
DevMilovici <devmilovici@gmail.com>
2025-02-12 12:09:37 +0200

    navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,7 @@
 <template>
   <div class="w-full">
-    <!-- Page header -->
-    <header class="min-w-[200px] flex flex-row gap-2 p-2 shadow-lg shadow-green-500/50">
-      <!-- Logo -->
-      <div class="flex flex-row items-center justify-center gap-2">
-        <!-- Image -->
-        <img alt="GeoFlowCraft logo" class="logo" src="@/assets/planet_icon_512x512.png" width="50" height="50" />
-        <!-- Title -->
-        <RouterLink to="/"><span class="font-bold text-2xl text-teal-400">GeoFlowCraft</span></RouterLink>
-      </div>
-      <!-- Nav links -->
-      <!-- <div class="h-full">
-        <nav class="flex flex-row items-center justify-center">
-          <RouterLink to="/">Home</RouterLink>
-          <RouterLink to="/about">About</RouterLink>
-        </nav>
-      </div> -->
-    </header>
-
+    <!-- Navbar -->
+    <AppNavBar/>
     <!-- Page content -->
     <RouterView class="w-full" />
   </div>
@@ -25,12 +9,14 @@
 
 <script>
 import { RouterLink, RouterView } from 'vue-router'
+import AppNavBar from "@/components/AppNavBar.vue";
 
 
 export default {
   name: "App",
   components: {
-    RouterLink, RouterView
+    RouterLink, RouterView,
+    AppNavBar
   },
   computed: {},
   methods: {}
@@ -40,17 +26,5 @@ export default {
 
 <style lang="scss">
 @import url("../node_modules/ol/ol.css");
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a:first-of-type {
-  border: 0;
-}
 
 </style>
---
+++ frontend/src/components/AppNavBar.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <nav class="min-w-[200px] flex flex-row items-center gap-2 p-2 shadow-lg shadow-green-500/50">
+            <!-- Logo -->
+            <div class="flex flex-row items-center justify-center gap-2">
+                <!-- Image -->
+                <img alt="GeoFlowCraft logo" class="logo" src="@/assets/planet_icon_512x512.png" width="50"
+                    height="50" />
+                <!-- Title -->
+                <RouterLink to="/"><span class="font-bold text-2xl text-teal-400">GeoFlowCraft</span></RouterLink>
+            </div>
+            <!-- Nav links -->
+            <div class="h-full border border-gray-700">
+                <nav class="flex flex-row items-center justify-center">
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+                </nav>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+import { RouterLink } from 'vue-router'
+
+export default {
+    name: "AppNavBar",
+    components: {
+        RouterLink
+    },
+    computed: {},
+    methods: {}
+}
+
+</script>
+
+<style lang="scss">
+nav a.router-link-exact-active {
+  color: rgb(20 184 166);;
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+</style>
