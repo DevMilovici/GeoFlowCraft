3d077abee93f193bcce5c75cbc3991a1f5dfd319
DevMilovici <devmilovici@gmail.com>
2025-02-13 10:43:48 +0200

    dataset list component

---
+++ frontend/src/components/AppDataSetList.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="flex flex-col items-center justify-center p-1">
+        <div class="flex flex-row gap-2 items-center justify-between p-2">
+            <div class="flex flex-row gap-2">
+                <i class="pi pi-database" style="font-size: 1.5rem;"></i>
+                <span class="w-full text-center">
+                    Datasets
+                </span>
+            </div>
+            <PrimeButton rounded type="button" icon="pi pi-plus" severity="success" size="small"
+                v-tooltip.bottom="'Create dataset'" />
+        </div>
+        <div class="flex flex-col gap-1 w-full">
+            <PrimeListBox v-model="selectedDataSet" :options="dataSets" optionLabel="name" fluid class="w-full">
+                <template #option="slotProps">
+                    <div class="flex items-center">
+                        <div>{{ slotProps.option.name }}</div>
+                    </div>
+                </template>
+            </PrimeListBox>
+        </div>
+    </div>
+    <!-- DataSets controls -->
+    <div class="w-full flex gap-1 items-center justify-center">
+        <PrimeButton :disabled="selectedDataSet === null" type="button" severity="info" label="Details" icon="pi pi-eye"
+            size="small" class="text-xs" />
+    </div>
+</template>
+
+<script>
+import useDataSetStore from "@/stores/dataSet";
+
+export default {
+    name: "AppDataSetList",
+    data() {
+        return {
+            selectedDataSet: null,
+            dataSets: [
+                {
+                    name: "DataSet_0",
+                    description: "This is DataSet_0...",
+                    layers: []
+                },
+                {
+                    name: "DataSet_1",
+                    description: "This is DataSet_0...",
+                    layers: []
+                },
+                {
+                    name: "DataSet_2",
+                    description: "This is DataSet_0...",
+                    layers: []
+                },
+                {
+                    name: "DataSet_3",
+                    description: "This is DataSet_0...",
+                    layers: []
+                },
+                {
+                    name: "DataSet_4",
+                    description: "This is DataSet_0...",
+                    layers: []
+                },
+                {
+                    name: "DataSet_5",
+                    description: "This is DataSet_0...",
+                    layers: []
+                },
+                {
+                    name: "DataSet_6",
+                    description: "This is DataSet_0...",
+                    layers: []
+                },
+                {
+                    name: "DataSet_7",
+                    description: "This is DataSet_0...",
+                    layers: []
+                },
+                {
+                    name: "DataSet_8",
+                    description: "This is DataSet_0...",
+                    layers: []
+                }
+            ]
+        }
+    },
+    async mounted() {
+        const dataSetStore = useDataSetStore();
+        this.dataSets.length = 0;
+        this.dataSets.push(...await dataSetStore.getDataSets());
+    }
+}
+
+</script>
+
+<style lang="scss">
+
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,32 +2,7 @@
 
   <div id="map" class="relative h-full">
     <div class="absolute z-[100] right-1 top-1 p-1 bg-gray-800 w-[250px] border-2 border-gray-300 rounded-md">
-      <!-- DataSets list -->
-      <div class="flex flex-col items-center justify-center p-1">
-        <div class="flex flex-row gap-2 items-center justify-between p-2">
-          <div class="flex flex-row gap-2">
-            <i class="pi pi-database" style="font-size: 1.5rem;"></i>
-            <span class="w-full text-center">
-              Datasets
-            </span>
-          </div>
-          <PrimeButton rounded type="button" icon="pi pi-plus" severity="success" size="small" />
-        </div>
-        <div class="flex flex-col gap-1 w-full">
-          <PrimeListBox v-model="selectedDataset" :options="datasets" optionLabel="name" fluid class="w-full">
-            <template #option="slotProps">
-              <div class="flex items-center">
-                <div>{{ slotProps.option.name }}</div>
-
-              </div>
-            </template>
-          </PrimeListBox>
-        </div>
-      </div>
-      <!-- DataSets controls -->
-      <div class="w-full flex gap-1 items-center justify-center">
-        <PrimeButton :disabled="selectedDataset === null" type="button" label="Details" icon="pi pi-eye" severity="info" size="small" class="text-xs"/>
-      </div>
+      <AppDataSetList />
     </div>
   </div>
 
@@ -39,60 +14,13 @@ import OSM from 'ol/source/OSM.js';
 import TileLayer from 'ol/layer/Tile.js';
 import View from 'ol/View.js';
 
+import AppDataSetList from "../components/AppDataSetList.vue";
+
 export default {
   name: "HomeView",
-  components: {},
+  components: { AppDataSetList },
   data() {
-    return {
-      selectedDataset: null,
-      datasets: [
-        {
-          name: "DataSet_0",
-          description: "This is DataSet_0...",
-          layers: []
-        },
-        {
-          name: "DataSet_1",
-          description: "This is DataSet_0...",
-          layers: []
-        },
-        {
-          name: "DataSet_2",
-          description: "This is DataSet_0...",
-          layers: []
-        },
-        {
-          name: "DataSet_3",
-          description: "This is DataSet_0...",
-          layers: []
-        },
-        {
-          name: "DataSet_4",
-          description: "This is DataSet_0...",
-          layers: []
-        },
-        {
-          name: "DataSet_5",
-          description: "This is DataSet_0...",
-          layers: []
-        },
-        {
-          name: "DataSet_6",
-          description: "This is DataSet_0...",
-          layers: []
-        },
-        {
-          name: "DataSet_7",
-          description: "This is DataSet_0...",
-          layers: []
-        },
-        {
-          name: "DataSet_8",
-          description: "This is DataSet_0...",
-          layers: []
-        }
-      ]
-    }
+    return {}
   },
   mounted() {
     // Taken from:  https://openlayers.org/doc/quickstart.html
@@ -114,11 +42,6 @@ export default {
 }
 </script>
 
-<style lang="scss">
-
-.p-listbox-list,	.p-listbox-option {
-  border: 1px solid rgb(79, 79, 79) !important;
-  background-color: rgb(31 41 55);
-}
+<style lang="scss" scoped>
 
 </style>
