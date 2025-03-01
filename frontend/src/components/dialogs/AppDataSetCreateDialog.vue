ad40f5a8bfff2319973747e5e5623c3f90009f3b
DevMilovici <devmilovici@gmail.com>
2025-03-01 21:50:40 +0200

    small identation update

--- frontend/src/components/dialogs/AppDataSetCreateDialog.vue
+++ frontend/src/components/dialogs/AppDataSetCreateDialog.vue
@@ -51,7 +51,9 @@ export default {
             },
             resolver: yupResolver(
                 yup.object().shape({
-                    name: yup.string().required("Name is required!").min(2, "Name must have at least 2 characters"),
+                    name: yup.string()
+                            .required("Name is required!")
+                            .min(2, "Name must have at least 2 characters1"),
                     description: yup.string()
                 })
             )
