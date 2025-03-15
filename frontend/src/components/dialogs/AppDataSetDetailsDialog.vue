a78eacd5eefd466eeef169a0e09d9b4532df2849
DevMilovici <devmilovici@gmail.com>
2025-03-15 17:45:49 +0200

    show/hide datalayer

--- frontend/src/components/dialogs/AppDataSetDetailsDialog.vue
+++ frontend/src/components/dialogs/AppDataSetDetailsDialog.vue
@@ -19,12 +19,27 @@
                         <div class="w-full flex flex-row gap-2 items-center justify-between">
                             <div>{{ slotProps?.option?.name }}</div>
                             <div class="flex flex-row items-center">
+                                <PrimeButton 
+                                    v-if="visibleLayers?.indexOf(slotProps?.option?.id) >= 0"
+                                    icon="pi pi-eye" 
+                                    variant="text" rounded 
+                                    v-tooltip.bottom="'Hide layer'"
+                                    @click="hideLayerOnMap(slotProps?.option)"
+                                />
+                                <PrimeButton 
+                                    v-else
+                                    icon="pi pi-eye-slash" 
+                                    variant="text" rounded 
+                                    v-tooltip.bottom="'Show layer on map'"
+                                    @click="showLayerOnMap(slotProps?.option)"
+                                />
                                 <i class="pi pi-info-circle" v-tooltip.bottom="slotProps?.option?.description"></i>
                             </div>
                         </div>
                     </template>
                 </PrimeListBox>
                 <div class="w-full flex gap-2 justify-center items-center">
+                    <PrimeToast />
                     <PrimeButton rounded type="button" icon="pi pi-plus" severity="success" size="small"
                         v-tooltip.bottom="'Add new layer'" variant="text" 
                         @click="showCreateDataLayerCatalog" />
@@ -45,6 +60,7 @@
 import { mapState } from 'pinia';
 import useDialogStore from "@/stores/dialog";
 import useDataSetStore from "@/stores/dataSet";
+import useMapStore from '@/stores/map';
 
 export default {
     name: "AppDataSetDetailsDialog",
@@ -56,12 +72,29 @@ export default {
     },
     computed: {
         ...mapState(useDialogStore, ["dataSetDetailsDialogVisible"]),
-        ...mapState(useDataSetStore, ["selectedDataSet"])
+        ...mapState(useDataSetStore, ["selectedDataSet"]),
+        ...mapState(useMapStore, ["visibleLayers"])
     },
     methods: {
         showCreateDataLayerCatalog() {
             const dialogStore = useDialogStore();
             dialogStore.showDataLayerCreateCatalog();
+        },
+        async showLayerOnMap(layerInfo) {
+            try {                
+                const mapStore = useMapStore();
+                await mapStore.addLayer({ id: layerInfo.id }, true);
+            } catch (error) {
+                this.$toast.add({ severity: "error", summary: "Datalayer display failed!", detail: `Something went wrong: "${error}".` , life: 3000 });
+            }
+        },
+        async hideLayerOnMap(layerInfo) {
+            try {
+                const mapStore = useMapStore();
+                mapStore.hideLayer(layerInfo.id);
+            } catch (error) {
+                this.$toast.add({ severity: "error", summary: "Datalayer hide failed!", detail: `Something went wrong: "${error}".` , life: 3000 });
+            }
         },
         close() {
             const dialogStore = useDialogStore();
